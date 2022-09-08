66ade3321dac68aa457c55eff9d27e7f5189bfe6
leonelborgesdev <leonel03t@gmail.com>
2022-09-08 13:30:13 -0400

    pokeapp

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
-  );
+  return <div className="App">pokeApp</div>;
 }
 
 export default App;
