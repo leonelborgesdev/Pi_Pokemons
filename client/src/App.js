6ff42420ebf17dee6529f0bc15cc6016c27da215
leonelborgesdev <leonel03t@gmail.com>
2022-09-12 14:26:08 -0400

    pokemons

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Pokemons } from "./components/Pokemons/Pokemons";
 
 function App() {
-  return <div className="App">pokeApp</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/pokemons" element={<Pokemons />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,21 @@
+import { GET_ALL_POKEMONS } from "../action/type";
+
+const initialState = {
+  pokemons: [],
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALL_POKEMONS:
+      return {
+        ...state,
+        pokemons: action.payload,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+export default rootReducer;
