294dc7a2a2747f60b9cddb735eb6c16be759105d
leonelborgesdev <leonel03t@gmail.com>
2022-09-15 12:07:24 -0400

    pokemons

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Pokemons } from "./components/Pokemons/Pokemons";
+import { Landing } from "./components/Landing/Landing";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route exact path="/" element={<Landing />} />
         <Route path="/pokemons" element={<Pokemons />} />
       </Routes>
     </div>
---
+++ client/src/components/Landing/Landing.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Landing.css";
+
+export const Landing = () => {
+  return <div>Landing</div>;
+};
