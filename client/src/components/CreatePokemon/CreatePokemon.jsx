d59ced0ba2a4f192668e1584a557b2db31dffcb6
leonelborgesdev <leonel03t@gmail.com>
2022-09-18 21:24:32 -0400

    pokemon

---
+++ client/src/components/CreatePokemon/CreatePokemon.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const CreatePokemon = () => {
+  return (
+    <div>
+      <h1>Create Pokemon</h1>
+      <div>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Life" />
+        <input type="text" placeholder="Strength" />
+        <input type="text" placeholder="Defending" />
+        <input type="text" placeholder="Speed" />
+        <input type="text" placeholder="Heigth" />
+        <input type="text" placeholder="Weight" />
+      </div>
+    </div>
+  );
+};
---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Nav.css";
+
+export const Nav = () => {
+  return <div>Nav</div>;
+};
