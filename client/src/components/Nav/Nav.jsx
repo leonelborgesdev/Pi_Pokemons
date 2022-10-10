5ba34ebe9fc92b08b571e8d9737a361c1e3b5759
leonelborgesdev <leonel03t@gmail.com>
2022-10-10 18:11:06 -0400

    pokemons

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -4,7 +4,10 @@ import { Link } from "react-router-dom";
 import { getPokmeonsByName } from "../../redux/action";
 import pokemon_icon2 from "../../assets/pokemon_icon2.png";
 import pokemon_icon from "../../assets/pokemon_icon.png";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 import "./Nav.css";
+import { useState } from "react";
 
 export const Nav = () => {
   const dispatch = useDispatch();
@@ -14,17 +17,59 @@ export const Nav = () => {
       dispatch(getPokmeonsByName(value));
     }
   };
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => {
+    setSidebar(!sidebar);
+  };
   return (
-    <div className="container_nav">
-      <div className="container_nav_home">
-        <Link to="/">
-          <img src={pokemon_icon} alt="app" height={"50"} />
+    <>
+      <div className="container_nav">
+        <Link to={"#"} className={"menu-bars"}>
+          <FaIcons.FaBars onClick={showSidebar} />
         </Link>
-        <Link to="/pokemons">
-          <img src={pokemon_icon2} alt="app" height={"50"} />
-        </Link>
+        <div className="container_nav_home">
+          <Link to="/">
+            <img src={pokemon_icon} alt="app" height={"50"} />
+          </Link>
+          <Link to="/pokemons">
+            <img src={pokemon_icon2} alt="app" height={"50"} />
+          </Link>
+        </div>
+        <input type="text" placeholder="Name" onKeyDown={handleSearch} />
       </div>
-      <input type="text" placeholder="Name" onKeyDown={handleSearch} />
-    </div>
+      <div className={sidebar ? "nav_menu active" : "nav_menu"}>
+        <ul className="nav-menu-items">
+          <li className="navbar-toogle">
+            <Link to={"#"} className={"menu-bars"}>
+              <AiIcons.AiOutlineClose />
+            </Link>
+          </li>
+          <li className="navbar-toogle">
+            <Link to={"#"} className={"menu-bars"}>
+              <AiIcons.AiOutlineClose />
+              <span>Types</span>
+            </Link>
+          </li>
+          <li className="navbar-toogle">
+            <Link to={"#"} className={"menu-bars"}>
+              <AiIcons.AiOutlineClose />
+              <span>Ordenar</span>
+            </Link>
+          </li>
+          <li className="navbar-toogle">
+            <Link to={"#"} className={"menu-bars"}>
+              <AiIcons.AiOutlineClose />
+              <span>Crear</span>
+            </Link>
+          </li>
+          <li className="navbar-toogle">
+            <Link to={"#"} className={"menu-bars"}>
+              <AiIcons.AiOutlineClose />
+              <span>Types</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
