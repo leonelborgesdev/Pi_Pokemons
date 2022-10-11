0f491b72e4c4c6805a9bcd8538246b872286d783
leonelborgesdev <leonel03t@gmail.com>
2022-10-10 23:17:41 -0400

    pokemons

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -8,6 +8,7 @@ import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import "./Nav.css";
 import { useState } from "react";
+import { IconContext } from "react-icons";
 
 export const Nav = () => {
   const dispatch = useDispatch();
@@ -23,50 +24,54 @@ export const Nav = () => {
   };
   return (
     <>
-      <div className="container_nav">
-        <div className="container_nav_home">
-          <Link to="/">
-            <img src={pokemon_icon} alt="app" height={"50"} />
-          </Link>
-          <Link to="/pokemons">
-            <img src={pokemon_icon2} alt="app" height={"50"} />
-          </Link>
-        </div>
-        <input type="text" placeholder="Name" onKeyDown={handleSearch} />
-        <Link to={"#"} className={"menu-bars"}>
-          <FaIcons.FaBars onClick={showSidebar} />
-        </Link>
-      </div>
-      <div className={sidebar ? "nav_menu active" : "nav_menu"}>
-        <ul className="nav-menu-items">
-          <li className="navbar-toogle">
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="container_nav">
+          <div className="container_nav_home">
+            <Link to="/">
+              <img src={pokemon_icon} alt="app" height={"50"} />
+            </Link>
+            <Link to="/pokemons">
+              <img src={pokemon_icon2} alt="app" height={"50"} />
+            </Link>
+          </div>
+          <input type="text" placeholder="Name" onKeyDown={handleSearch} />
+          <div className="contenedor_toogle">
             <Link to={"#"} className={"menu-bars"}>
-              <AiIcons.AiOutlineClose />
+              <FaIcons.FaBars onClick={showSidebar} />
             </Link>
-          </li>
-          <li className="nav-text">
-            <Link to={"#"}>
-              <span>Types</span>
-            </Link>
-          </li>
-          <li className="nav-text">
-            <Link to={"#"}>
-              <span>Ordenar</span>
-            </Link>
-          </li>
-          <li className="nav-text">
-            <Link to={"#"}>
-              <span>Alfabetico</span>
-            </Link>
-          </li>
-          <li className="nav-text">
-            <Link to={"#"}>
-              <AiIcons.AiFillPlusCircle />
-              <span>Crear</span>
-            </Link>
-          </li>
-        </ul>
-      </div>
+          </div>
+        </div>
+        <div className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items">
+            <li className="navbar-toogle">
+              <Link to={"#"} className={"menu-bars"}>
+                <AiIcons.AiOutlineClose onClick={showSidebar} />
+              </Link>
+            </li>
+            <li className="nav-text">
+              <Link to={"#"}>
+                <span>Types</span>
+              </Link>
+            </li>
+            <li className="nav-text">
+              <Link to={"#"}>
+                <span>Ordenar</span>
+              </Link>
+            </li>
+            <li className="nav-text">
+              <Link to={"#"}>
+                <span>Alfabetico</span>
+              </Link>
+            </li>
+            <li className="nav-text">
+              <Link to={"#"}>
+                <AiIcons.AiFillPlusCircle />
+                <span>Crear</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </IconContext.Provider>
     </>
   );
 };
