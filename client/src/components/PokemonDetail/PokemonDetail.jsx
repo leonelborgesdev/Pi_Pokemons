dcaee5d0e4db13c99c489ba2dab8a1d7e6f30142
leonelborgesdev <leonel03t@gmail.com>
2023-05-04 09:51:05 -0400

    pokemon

--- client/src/components/PokemonDetail/PokemonDetail.jsx
+++ client/src/components/PokemonDetail/PokemonDetail.jsx
@@ -12,21 +12,34 @@ export const PokemonDetail = () => {
   useEffect(() => {
     dispatch(getPokemonsById(id));
   }, []);
+  const bagr = "grass";
   return (
     <div className="container_id">
       <h1>Detail Pokemons</h1>
-      <div className="container_id_detail">
+      <div className="container_id_detail" id={bagr}>
         <div className="container_id_img">
           <img src={pokemon.sprite ? pokemon.sprite : pokemon.sprite2} alt="" />
         </div>
         <div className="container_id_description">
-          <h2>{pokemon.name}</h2>
-          <h3>Life:{pokemon.life}</h3>
-          <h3>Strength:{pokemon.strength}</h3>
-          <h3>Defending:{pokemon.defending}</h3>
-          <h3>Speed:{pokemon.speed}</h3>
-          <h3>Height:{pokemon.height}</h3>
-          <h3>Weight:{pokemon.weight}</h3>
+          {pokemon !== undefined ? (
+            <React.Fragment>
+              <h2>{pokemon.name}</h2>
+              <h3>Life:{pokemon.life}</h3>
+              <h3>Strength:{pokemon.strength}</h3>
+              <h3>Defending:{pokemon.defending}</h3>
+              <h3>Speed:{pokemon.speed}</h3>
+              <h3>Height:{pokemon.height}</h3>
+              <h3>Weight:{pokemon.weight}</h3>
+              <h3>
+                Type:
+                {pokemon.types !== undefined &&
+                  pokemon.types.map((type) => type.name)}
+              </h3>
+              {console.log(pokemon.types)}
+            </React.Fragment>
+          ) : (
+            <h3>NO se encontro pokemon</h3>
+          )}
           <Link to={"/pokemons"}>Volver</Link>
         </div>
       </div>
